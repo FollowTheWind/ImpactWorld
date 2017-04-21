24f251a74c728d24f6e87dffb234acd2c1666e9e
shining_sir <shining_man@163.com>
2017-04-21 15:34:47 +0800

    Init

--- Assets/_ImpactWorld/Scripts/PlayerMovement.cs
+++ Assets/_ImpactWorld/Scripts/PlayerMovement.cs
@@ -1,16 +1,87 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static PlayerMovement instance;
+
+    private bool isJump;
+    public float jumpDistance;
+    public float jumpPower;
+    public float jumpDuration;
+    public float moveDuration;
+
+
+    void Awake()
+    {
+        instance = this;
+        isJump = true;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            HorizontalMove();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerJump();
+        }
+
+    }
+
+    /// <summary>
+    /// Control the player jump
+    /// </summary>
+    private void PlayerJump()
+    {
+        if (isJump)
+        {
+            isJump = false;
+            transform.DOKill();
+            Vector3 pos = transform.position;
+            pos += transform.forward * jumpDistance;
+            transform.DOJump(new Vector3(pos.x, transform.position.y, pos.z), jumpPower, 1, jumpDuration)
+                .OnComplete( () => {
+                    isJump = true;
+                });
+        }
+        
+    }
+
+    /// <summary>
+    /// Move along the XZ plane
+    /// </summary>
+    private void HorizontalMove()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            print("击中物体 tag " + hit.transform.tag);
+
+            if (hit.transform.tag == "Ground")
+            {
+                print("击中地面");
+
+                if (isJump)
+                {
+                    transform.DOLookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z), 0.3f);
+                    transform.DOMove(new Vector3(hit.point.x, transform.position.y, hit.point.z), moveDuration);
+                }                
+            }
+            else
+            {
+                print("不是地面");
+            }
+        }
+    }
+
 }
